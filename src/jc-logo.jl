273b6f3935e18171ba1ffb191a909b7bba8bf4dd
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-22 22:48:03 -0300

    add punctured version

--- src/jc-logo.jl
+++ src/jc-logo.jl
@@ -13,39 +13,44 @@ function drawlogo(logo_width::Int)
     origin() # Set origin to center of image
     scale(s) # Call to scale() must come after origin()
 
-    ref_dist = 320
+    triangle_side = 0.73*base_width
+    triangle_height = triangle_side*√3/4
 
-    ball_centers = [Point(-ref_dist,ref_dist), Point(0,-ref_dist), Point(ref_dist,ref_dist)]
-    ball_radius = div(ref_dist,2)
+    ball_centers = [Point(-triangle_side/2,triangle_height), Point(0,-triangle_height), Point(triangle_side/2,triangle_height)]
+    ball_radius = 0.18*triangle_side
     ball_colors = ["brown3", "forestgreen", "mediumorchid3"]
 
-    link_small_thickness = div(ref_dist,16)*s # line thickness is not scaled http://juliagraphics.github.io/Luxor.jl/stable/transforms/#Scaling-of-lines
-    link_large_length = div(ref_dist, 6)
-    link_large_width = div(ref_dist, 8)
-    link_num = 3 # number of large chains in each direction
+    link_num = 7 # number of chain links in each direction    
+    link_small_width = triangle_side/48
+    link_large_width = triangle_side/24
+    link_thickness = 2*link_small_width*s # line thickness is not scaled http://juliagraphics.github.io/Luxor.jl/stable/transforms/#Scaling-of-lines
+    link_length = triangle_side/link_num/2 + link_small_width/2
+    
+    
     link_color = "royalblue"
 
-    @layer begin # Draw triangle contour for a "small chain links" effect
-        setcolor(link_color)
-        setline(link_small_thickness)
-        poly(ball_centers, :stroke, close=true)
-    end
-    
     @layer begin # Draw rounded rectangles for the "large chain links"
         setcolor(link_color)
-        angles = (-atan(2), atan(2), 0)
+        angles = (-π/3, π/3, 0)
         for i in 1:3
             pt1 = ball_centers[mod1(i, 3)]
             pt2 = ball_centers[mod1(i + 1, 3)]
-            for j in 1:link_num
-                link_large_shape = squircle(O, link_large_length, link_large_width, rt=0.5, vertices=true)
-                link_large = poly(link_large_shape, :transparent)
+            for j in 1:link_num+1
+                if mod(j,2) == 1
+                    link_shape = squircle(O, link_length, link_small_width, rt=0.5, vertices=true)
+                    style = :fill
+                else
+                    link_shape = squircle(O, link_length, link_large_width, rt=0.5, vertices=true)
+                    style = :stroke
+                end
+                link_large = poly(link_shape, :transparent)
                 polyrotate!(link_large, angles[i])
                 polymove!(link_large, O, between(pt1, pt2, j/(link_num+1)))
-                poly(link_large, :fill)
+                setline(link_thickness)
+                poly(link_large, style)
             end
         end
-    end
+    end    
 
     @layer begin # Draw "chain" balls        
         for (ball_center, ball_color) in zip(ball_centers, ball_colors)
@@ -53,11 +58,10 @@ function drawlogo(logo_width::Int)
             circle(ball_center, ball_radius, :fill)
         end
     end    
-
-    finish()
 end
 
 for logo_width in (1024, 512, 128, 32) # large, medium, small and favicon sizes
     Drawing(logo_width, logo_width, joinpath("logo_images", "jc-logo_"*string(logo_width)*"x"*string(logo_width)*".png"))
     drawlogo(logo_width)
+    finish()
 end
---
+++ src/jc-logo_solid.jl
@@ -0,0 +1,63 @@
+using Luxor
+
+"""
+    drawlogo(logo_width::Int)
+
+Draw a `logo_width × logo_width`-sized square image logo for JuliaConstraints.
+"""
+function drawlogo(logo_width::Int)
+
+    base_width = 1024
+    s = logo_width/base_width
+
+    origin() # Set origin to center of image
+    scale(s) # Call to scale() must come after origin()
+
+    ref_dist = 320
+
+    ball_centers = [Point(-ref_dist,ref_dist), Point(0,-ref_dist), Point(ref_dist,ref_dist)]
+    ball_radius = div(ref_dist,2)
+    ball_colors = ["brown3", "forestgreen", "mediumorchid3"]
+
+    link_small_thickness = div(ref_dist,16)*s # line thickness is not scaled http://juliagraphics.github.io/Luxor.jl/stable/transforms/#Scaling-of-lines
+    link_large_length = div(ref_dist, 5)
+    link_large_width = div(ref_dist, 7)
+    link_num = 3 # number of large chains in each direction
+    link_color = "royalblue"
+
+    @layer begin # Draw triangle contour for a "small chain links" effect
+        setcolor(link_color)
+        setline(link_small_thickness)
+        poly(ball_centers, :stroke, close=true)
+    end
+
+    @layer begin # Draw rounded rectangles for the "large chain links"
+        setcolor(link_color)
+        angles = (-atan(2), atan(2), 0)
+        for i in 1:3
+            pt1 = ball_centers[mod1(i, 3)]
+            pt2 = ball_centers[mod1(i + 1, 3)]
+            for j in 1:link_num
+                link_large_shape = squircle(O, link_large_length, link_large_width, rt=0.5, vertices=true)
+                link_large = poly(link_large_shape, :transparent)
+                polyrotate!(link_large, angles[i])
+                polymove!(link_large, O, between(pt1, pt2, j/(link_num+1)))
+                setline(link_small_thickness)
+                poly(link_large, :fill)
+            end
+        end
+    end
+
+    @layer begin # Draw "chain" balls        
+        for (ball_center, ball_color) in zip(ball_centers, ball_colors)
+            setcolor(ball_color)
+            circle(ball_center, ball_radius, :fill)
+        end
+    end
+end
+
+for logo_width in (1024, 512, 128, 32) # large, medium, small and favicon sizes
+    Drawing(logo_width, logo_width, joinpath("logo_images", "jc-logo_solid_"*string(logo_width)*"x"*string(logo_width)*".png"))
+    drawlogo(logo_width)
+    finish()
+end
