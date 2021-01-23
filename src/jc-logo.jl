f14fbd4f62c1ae2de9b74543d44ff0c2d5196d85
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-22 23:47:56 -0300

    cosmetics and comments

--- src/jc-logo.jl
+++ src/jc-logo.jl
@@ -14,9 +14,9 @@ function drawlogo(logo_width::Int)
     scale(s) # Call to scale() must come after origin()
 
     triangle_side = 0.73*base_width
-    triangle_height = triangle_side*√3/4
+    triangle_height = triangle_side*√3/2
 
-    ball_centers = [Point(-triangle_side/2,triangle_height), Point(0,-triangle_height), Point(triangle_side/2,triangle_height)]
+    ball_centers = [Point(-triangle_side/2,triangle_height/2), Point(0,-triangle_height/2), Point(triangle_side/2,triangle_height/2)]
     ball_radius = 0.18*triangle_side
     ball_colors = ["brown3", "forestgreen", "mediumorchid3"]
 
@@ -35,24 +35,24 @@ function drawlogo(logo_width::Int)
         for i in 1:3
             pt1 = ball_centers[mod1(i, 3)]
             pt2 = ball_centers[mod1(i + 1, 3)]
-            for j in 1:link_num+1
+            for j in 1:link_num
                 if mod(j,2) == 1
-                    link_shape = squircle(O, link_length, link_small_width, rt=0.5, vertices=true)
+                    link_shape = squircle(O, link_length, link_small_width, rt=0.5, vertices=true) # no drawing, just build array with shape
                     style = :fill
                 else
-                    link_shape = squircle(O, link_length, link_large_width, rt=0.5, vertices=true)
+                    link_shape = squircle(O, link_length, link_large_width, rt=0.5, vertices=true) # no drawing, just build array with shape
                     style = :stroke
                 end
-                link_large = poly(link_shape, :transparent)
-                polyrotate!(link_large, angles[i])
-                polymove!(link_large, O, between(pt1, pt2, j/(link_num+1)))
+                link_large = poly(link_shape, :transparent) # transparent => no drawing
+                polyrotate!(link_large, angles[i]) # rotate transparent polygonal, so no drawing
+                polymove!(link_large, O, between(pt1, pt2, j/(link_num+1))) # move transparent polygonal, still no drawing
                 setline(link_thickness)
-                poly(link_large, style)
+                poly(link_large, style) # finally draws it, in the right place
             end
         end
     end    
 
-    @layer begin # Draw "chain" balls        
+    @layer begin # Draw balls
         for (ball_center, ball_color) in zip(ball_centers, ball_colors)
             setcolor(ball_color)
             circle(ball_center, ball_radius, :fill)
